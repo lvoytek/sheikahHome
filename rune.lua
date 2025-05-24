5971c69ff13a10ae1c9c52aacba5ff3bbb6fbd00
Lena Voytek <lena@voytek.dev>
2025-05-24 17:19:50 -0400

    Draw rune using canvas

--- rune.lua
+++ rune.lua
@@ -11,20 +11,26 @@ function Rune:load()
 end
 
 -- Create a new Rune instance
-function Rune:new()
+function Rune:new(width)
     local self = setmetatable({}, Rune)
     self.selected = false
+    self.width = width or 256
+    self.canvas = love.graphics.newCanvas(self.width, self.width)
+    self.needsUpdate = true
     return self
 end
 
 -- Mark this Rune as selected
 function Rune:select()
     self.selected = true
+    self.needsUpdate = true
 end
 
 -- Mark this Rune as deselected
 function Rune:deselect()
     self.selected = false
+    self.needsUpdate = true
+
 end
 
 function Rune:update(dt)
@@ -32,11 +38,24 @@ end
 
 -- Draw the rune and its app icon
 function Rune:draw(x, y)
-    if self.selected then
-        love.graphics.draw(rune_background_selected, x, y)
-    else
-        love.graphics.draw(rune_background, x, y)
+    if self.needsUpdate then
+        love.graphics.setCanvas(self.canvas)
+        love.graphics.clear()
+        love.graphics.setColor(1, 1, 1, 1)
+
+        if self.selected then
+            love.graphics.draw(rune_background_selected, 0, 0, 0, self.width / rune_background_selected:getWidth(), self.width / rune_background_selected:getHeight())
+        else
+            love.graphics.draw(rune_background, 0, 0, 0, self.width / rune_background:getWidth(), self.width / rune_background:getHeight())
+        end
+
+        self.needsUpdate = false
+        love.graphics.setCanvas()
     end
+
+    love.graphics.setBlendMode("alpha", "premultiplied")
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(self.canvas, x, y)
 end
 
 return Rune
