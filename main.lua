8f84d551c82e5746eb1358e19262a5ea13ed3c6a
Lena Voytek <lena@voytek.dev>
2025-05-25 00:52:57 -0400

    Add background image

--- main.lua
+++ main.lua
@@ -2,10 +2,13 @@ local application_connector = require("gio_application_connector")
 
 local runes
 local available_apps
+local background_image
 
 function love.load()
     love.window.setTitle("Home")
     love.window.setFullscreen(true)
+
+    background_image = love.graphics.newImage("img/background.png")
 
     local RuneManager = require("rune_manager")
     runes = RuneManager:new()
@@ -31,5 +34,8 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(background_image, 0, 0, 0, love.graphics.getWidth() / background_image:getWidth(), love.graphics.getHeight() / background_image:getHeight())
+
     runes:draw()
 end
