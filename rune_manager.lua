38c0fad438add3a86cdc525c220f7c677983a064
Lena Voytek <lena@voytek.dev>
2025-06-10 06:48:14 -0400

    Display app descriptions

--- rune_manager.lua
+++ rune_manager.lua
@@ -133,11 +133,19 @@ end
 -- Draw all runes
 function RuneManager:draw()
     if self.selectedRune ~= nil then
+        -- Display rune name and description
         local runeName = self.runes[self.selectedRune]:getAppName()
+        local runeDescription = self.runes[self.selectedRune]:getAppDescription()
         if runeName ~= "" then
             love.graphics.setColor(0x3C / 255, 0xD3 / 255, 0xFC / 255, 1)
             love.graphics.setFont(titleFont)
             love.graphics.print(runeName, love.graphics.getWidth() / 2 - titleFont:getWidth(runeName) / 2, love.graphics.getHeight() / 3)
+
+            -- Show description if it exists and is different from the name
+            if runeDescription ~= "" and runeDescription ~= runeName then
+                love.graphics.print(runeDescription, love.graphics.getWidth() / 2 - titleFont:getWidth(runeDescription) / 2, love.graphics.getHeight() * 2 / 3)
+            end
+
             love.graphics.setColor(1, 1, 1, 1)
         end
     end
